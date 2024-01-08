4771bb102c73a5f204bbf93865139ab2788fa7ad
sunting <sunting.huang@iyouke.com>
2024-01-09 07:58:24 +0800

    test

--- src/main/java/com/bravo/pattern/DesignPatternApplication.java
+++ src/main/java/com/bravo/pattern/DesignPatternApplication.java
@@ -3,6 +3,12 @@ package com.bravo.pattern;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 测试一下
+ *
+ * @author bravo1988
+ * @since 2024-01-09 07:58:06
+ */
 @SpringBootApplication
 public class DesignPatternApplication {
 
