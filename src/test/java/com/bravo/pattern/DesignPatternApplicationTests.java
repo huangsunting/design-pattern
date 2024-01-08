4ad2dd0608d49d25789ff8992df8aeb38f56364c
sunting <sunting.huang@iyouke.com>
2024-01-09 07:45:42 +0800

    init

---
+++ src/test/java/com/bravo/pattern/DesignPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bravo.pattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
