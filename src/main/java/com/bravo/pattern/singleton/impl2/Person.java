33edf6cb600bb65c4211befbcad6100d9079844e
sunting <sunting.huang@iyouke.com>
2024-01-15 17:45:33 +0800

    commit

--- src/main/java/com/bravo/pattern/singleton/impl2/Person.java
+++ src/main/java/com/bravo/pattern/singleton/impl2/Person.java
@@ -4,15 +4,14 @@ public class Person {
 
     private final String name;
 
-    // 私有构造器，防止外界new
     private Person(String name) {
         this.name = name;
     }
 
-    // 类加载时不初始化对象（所谓“懒”）
+    // 类加载时不初始化对象
     private static Person INSTANCE = null;
 
-    // 第一次调用时才初始化，后续调用由于instance已经创建，会直接返回
+    // 第一次调用时才初始化（所谓“懒”）
     public static Person getInstance() {
         if (INSTANCE == null) {
             INSTANCE = new Person("bravo1988");
--- src/main/java/com/bravo/pattern/singleton/impl3/Person.java
+++ src/main/java/com/bravo/pattern/singleton/impl3/Person.java
@@ -4,7 +4,6 @@ public class Person {
 
     private final String name;
 
-    // 私有构造器，防止外界new
     private Person(String name) {
         this.name = name;
     }
