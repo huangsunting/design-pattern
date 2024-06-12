42f4499a1096337e889c111ea6bfa9388a706016
sunting <sunting.huang@iyouke.com>
2024-06-12 10:14:28 +0800

    update comment

--- src/main/java/com/bravo/advanced/generics/genericmethod/ListUtil.java
+++ src/main/java/com/bravo/advanced/generics/genericmethod/ListUtil.java
@@ -4,8 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * 泛型类/泛型接口：在[类上]声明类型，在[类中]使用类型
- * 泛型方法：在[方法上]声明类型，在[方法中]使用类型
+ * ListUtil没有实例化的必要，声明泛型方法更为合适
+ * - 泛型类/泛型接口：在[类上]声明类型，在[类中]使用类型
+ * - 泛型方法：在[方法上]声明类型，在[方法中]使用类型
  */
 public class ListUtil /*<T>*/ {
 
@@ -19,13 +20,15 @@ public class ListUtil /*<T>*/ {
         return result;
     }
 
-    public <T, R> List<R> map(List<T> list, Mapper<T, R> mapper) {
+    public static <T, R> List<R> map(List<T> list, Mapper<T, R> mapper) {
         List<R> result = new ArrayList<>();
         for (T element : list) {
             result.add(mapper.mapping(element));
         }
         return result;
     }
+
+    // ------- 函数式接口，搭配上面的filter、map使用，也可以提取成单独的类 -------
 
     public interface Mapper<T, R> {
         R mapping(T item);
