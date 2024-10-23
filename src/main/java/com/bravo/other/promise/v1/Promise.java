2c3e30dc909f31ddbb836b3b64aba3149a3e265c
sunting <5450683+bravo1988@user.noreply.gitee.com>
2024-10-23 16:32:49 +0800

    Simple Promise for Java

---
+++ src/main/java/com/bravo/other/promise/v1/Promise.java
@@ -0,0 +1,117 @@
+package com.bravo.other.promise.v1;
+
+import lombok.Getter;
+
+/**
+ * 仿写js的Promise，极简版
+ * - 不支持链式处理多个任务
+ * - 仅支持处理异步任务，传入同步任务会导致结果无法回调
+ */
+public class Promise {
+
+    /**
+     * 异步结果处理器
+     */
+    private ResultHandler resultHandler;
+    /**
+     * 异常信息处理器
+     */
+    private ErrorHandler errorHandler;
+    /**
+     * 任务状态
+     */
+    private State state = State.PENDING;
+
+    /**
+     * 构造函数
+     * 1.接收任务
+     * 2.启动任务
+     */
+    public Promise(Task task) {
+        // 准备 resolve 和 reject 函数
+        Resolution resolution = new Resolution() {
+            @Override
+            public void resolve(Object obj) {
+                if (state != State.PENDING) {
+                    return;
+                }
+                state = State.FULFILLED;
+                if (resultHandler != null) {
+                    resultHandler.accept(obj);
+                }
+            }
+        };
+        Rejection rejection = new Rejection() {
+            @Override
+            public void reject(Object obj) {
+                if (state != State.PENDING) {
+                    return;
+                }
+                state = State.REJECTED;
+                if (errorHandler != null) {
+                    errorHandler.accept(obj);
+                }
+            }
+        };
+
+        try {
+            // 启动任务，同时传入 resolve 和 reject ，用于任务结束后通知promise进行下一步处理
+            task.start(resolution, rejection);
+        } catch (Exception e) {
+            rejection.reject(e);
+        }
+    }
+
+    /**
+     * 正常回调
+     */
+    public Promise then(ResultHandler resultHandler) {
+        this.resultHandler = resultHandler;
+        // 支持链式调用
+        return this;
+    }
+
+    /**
+     * 异常捕获
+     */
+    public void catching(ErrorHandler errorHandler) {
+        this.errorHandler = errorHandler;
+    }
+
+    @Override
+    public String toString() {
+        return "Promise{" +
+                "state=" + state +
+                '}';
+    }
+
+    // ------ 下面是辅助类 ------
+
+    @Getter
+    private enum State {
+        PENDING,
+        FULFILLED,
+        REJECTED;
+    }
+
+    public interface Task {
+        void start(Resolution r, Rejection rj);
+    }
+
+    public interface Resolution {
+        void resolve(Object obj);
+    }
+
+    public interface Rejection {
+        void reject(Object obj);
+    }
+
+    public interface ResultHandler {
+        void accept(Object result);
+    }
+
+    public interface ErrorHandler {
+        void accept(Object error);
+    }
+
+}
---
+++ src/main/java/com/bravo/other/promise/v1/PromiseTest.java
@@ -0,0 +1,108 @@
+package com.bravo.other.promise.v1;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+public class PromiseTest {
+
+    /**
+     * 模拟js的Promise，resolve场景
+     */
+    @Test
+    public void testAsyncTaskResolved() throws InterruptedException {
+        Promise p = new Promise((r, rj) -> {
+            setTimeout(() -> {
+                r.resolve("success");
+            }, 2000);
+        });
+
+        log.info("promise:{}", p);
+
+        p.then(result -> {
+            log.info("异步操作执行结果:{}", result);
+        }).catching(error -> {
+            log.info("异步操作执行结果:{}", error);
+        });
+
+        log.info("promise:{}", p);
+
+        // 等待异步执行完毕，重新观察promise的状态
+        TimeUnit.SECONDS.sleep(5);
+        log.info("promise:{}", p);
+    }
+
+    /**
+     * 模拟js的Promise，reject场景
+     */
+    @Test
+    public void testAsyncTaskRejected() throws InterruptedException {
+        new Promise((r, rj) -> {
+            setTimeout(() -> {
+                rj.reject("error");
+            }, 2000);
+        }).then(result -> {
+            log.info("异步操作执行结果:{}", result);
+        }).catching(error -> {
+            log.info("异步操作执行结果:{}", error);
+        });
+
+        TimeUnit.SECONDS.sleep(5);
+    }
+
+    /**
+     * 由于这里是另一个线程里的异常，当前线程无法捕获，所以catching逻辑
+     */
+    @Test
+    public void testAsyncTaskException() throws InterruptedException {
+        new Promise((r, rj) -> {
+            setTimeout(() -> {
+                throw new RuntimeException("runtime exception...");
+            }, 1000);
+        }).then(result -> {
+            log.info("异步操作执行结果:{}", result);
+        }).catching(error -> {
+            log.info("异步操作执行结果:{}", error);
+        });
+
+        TimeUnit.SECONDS.sleep(5);
+    }
+
+    /**
+     * BUG：如果构造函数传入的是同步任务，会立即执行then或catch，而此时then还没设置，会有问题
+     */
+    @Test
+    public void testSyncTask() {
+        Promise p = new Promise((r, rj) -> {
+            r.resolve("success");
+            rj.reject("error");
+        });
+
+        log.info("promise:{}", p);
+
+        p.then(result -> {
+            log.info("同步操作执行结果:{}", result);
+        }).catching(error -> {
+            log.info("同步操作执行结果:{}", error);
+        });
+
+        log.info("promise:{}", p);
+    }
+
+    /**
+     * 模拟js的setTimeout函数
+     */
+    private static void setTimeout(Runnable runnable, long delay) {
+        new Thread(() -> {
+            try {
+                TimeUnit.MILLISECONDS.sleep(delay);
+                runnable.run();
+            } catch (InterruptedException e) {
+                log.info("InterruptedException");
+            }
+        }).start();
+    }
+
+}
